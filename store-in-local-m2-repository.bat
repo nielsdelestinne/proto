1a215e56e969fbd0c8cc615b1993739985dcc4b0
nielsdelestinne <nielsdelestinne@gmail.com>
2020-11-02 21:06:17 +0100

    Add artifact (jar) with dependencies to Git + updated script to store jar in local m2

--- store-in-local-m2-repository.bat
+++ store-in-local-m2-repository.bat
@@ -1,2 +1,2 @@
 REM Install Jar Artifact into the local Maven repository (m2)
-mvn install:install-file -Dfile=employment-service-contract/artifacts/employment-service-contract-1.0.0.jar -DgroupId=be.niedel -DartifactId=employment-service-contract -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=employment-service-contract/artifacts/employment-service-contract-1.0.0-jar-with-dependencies.jar -DgroupId=be.niedel -DartifactId=employment-service-contract -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
